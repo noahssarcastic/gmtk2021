928fcbb7392bc9389b74d21577bfa0e7e5eeeda5
noahssarcastic <niniguezg@gmail.com>
2021-06-12 23:23:57 -0500

    Keep courage bar to side

--- Assets/Scripts/CourageBar.cs
+++ Assets/Scripts/CourageBar.cs
@@ -7,6 +7,8 @@ public class CourageBar : MonoBehaviour
 {
     private Slider slider;
     private RectTransform barTransform;
+    private float defaultXScale;
+    private float defaultXPosition;
     private float defaultWidth;
 
     private const float baseCourage = 100;
@@ -14,7 +16,9 @@ public class CourageBar : MonoBehaviour
     void Awake() {
         slider = GetComponent<Slider>();
         barTransform = GetComponent<RectTransform>();
-        defaultWidth = barTransform.transform.localScale.x;
+        defaultXScale = barTransform.transform.localScale.x;
+        defaultXPosition = barTransform.transform.position.x;
+        defaultWidth = barTransform.rect.width;
     }
 
     public void SetCourage(float percent) {
@@ -34,10 +38,21 @@ public class CourageBar : MonoBehaviour
         } else if (value > 200) {
             value = 200;
         }
+
         slider.maxValue = value;
+
+        float newScale = defaultXScale * (value / baseCourage);
         barTransform.localScale = new Vector3(
-            defaultWidth * (value / baseCourage), 
+            newScale, 
             barTransform.localScale.y, 
             barTransform.localScale.z);
+
+        float newWidth = defaultWidth * newScale;
+        float offset = (newWidth - defaultWidth) / 2;
+        barTransform.position = new Vector3(
+            defaultXPosition + offset,
+            barTransform.position.y,
+            barTransform.position.z
+        );
     }
 }
