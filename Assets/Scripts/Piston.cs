2813e602aa84d308a9ffff8f2430d004d49f848d
noahssarcastic <niniguezg@gmail.com>
2021-06-13 03:15:09 -0500

    Piston is mostly working

--- Assets/Scripts/Piston.cs
+++ Assets/Scripts/Piston.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Piston : MonoBehaviour
 {
     [SerializeField] private bool raising = false;
+    [SerializeField] private float height = 10;
+    [SerializeField] private float time = 0.01f;
+    private Vector3 startPosition;
+
+    void Awake() {
+        startPosition = gameObject.transform.position;
+    }
 
     void Update() {
         if (raising) {
@@ -16,13 +23,26 @@ public class Piston : MonoBehaviour
 
     private void Raise() {
         GetComponent<SpriteRenderer>().color = Color.green;
+        Vector3 destination = startPosition + Vector3.up * height;
+        Move(destination);
     }
 
     private void Lower() {
         GetComponent<SpriteRenderer>().color = Color.red;
+        Move(startPosition);
     }
 
     public void SetRaising(bool value) {
         raising = value;
+    }
+
+    private void Move(Vector3 destination) {
+        gameObject.transform.position = Vector3.Lerp(
+            gameObject.transform.position, 
+            destination, 
+            time);
+        // Snap when close enough to prevent annoying slerp behavior
+        if ((destination - gameObject.transform.position).magnitude <= 0.05f)
+            gameObject.transform.position = destination;
     }
 }
